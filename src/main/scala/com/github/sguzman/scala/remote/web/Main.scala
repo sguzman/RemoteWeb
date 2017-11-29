e4b052fc505109a0e5b9abde64245eb427d46b14
Salvador Guzman <guzmansalv@gmail.com>
2017-11-28 16:12:15 -0800

    added main stub

---
+++ src/main/scala/com/github/sguzman/scala/remote/web/Main.scala
@@ -0,0 +1,18 @@
+package com.github.sguzman.scala.remote.web
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import lol.http._
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val port = System.getenv("PORT")
+
+    Server.listen(8888) {
+      case e => handle(e)
+    }
+  }
+
+  def handle(e: Request) = {
+    Ok("good")
+  }
+}
